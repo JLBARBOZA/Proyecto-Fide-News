831bb209c40951f6abc395ec4f2df62163f8a94a
Lamec <Lamec@DESKTOP-HT67QNO>
2022-08-23 19:44:25 -0600

    Ultimo avance test

--- src/main/java/com/proyecto_WebPatrones/WebConfig.java
+++ src/main/java/com/proyecto_WebPatrones/WebConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /**
- *
+ *Hola
  * @author Lamec
  */
 @Configuration
